77646746f2a71ca46c558f15234ce524628570dd
Joao Vitor Ribeiro <joaovitordias.2b@gmail.com>
2023-08-17 16:31:16 -0300

    Projeto concluido

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 AOS.init();
+
+
+const dataDoEvento = new Date("May 21, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contador = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime(); 
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60 ;
+
+    const diaAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diaAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contador);
+        document.getElementById('contador').innerHTML =  "já acabou"
+    }
+}, 1000);
